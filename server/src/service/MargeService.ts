654f0a6b480e77a463218f83178fe0b9107bacb8
Anton <popsof@yandex.ru>
2024-08-26 16:40:38 +0300

    service logic upd 260824

--- server/src/service/MargeService.ts
+++ server/src/service/MargeService.ts
@@ -1,20 +1,56 @@
 import MargeType from "../type";
 
-class MargeService{ 
-
-   static async mathMarge(margeItems:any){ 
-
+class MargeService {
+    static async mathMarge(margeItems: MargeType): Promise<MargeType | Error> {
+      try {
         const Items = margeItems;
-
-        let AllItems: any = {}
-
-        if(Items[0].animals && Object.keys(Items[0].animals).length > 0){ 
-            AllItems.animals = Items[0].animals
+  
+        let AllItems: MargeType = {};
+  
+        for (const key in Items) {
+          if (Items.hasOwnProperty(key)) {
+            const currentArray = Items[key];
+  
+            if (currentArray && currentArray.length > 0) {
+              const calculatedArray = this.calculateItems(currentArray);
+              AllItems[key] = calculatedArray;
+            }
+          }
         }
+  
+        if (Object.keys(AllItems).length > 0) {
+          console.log(AllItems);
+        }
+  
+        return AllItems;
+      } catch (e: any) {
+        return e;
+      }
+    }
+  
+    static calculateItems(items: Array<{ [key: string]: number }>): Array<{ [key: string]: number }> {
+      let craftProgress: number = 0;
+      let currentProgress: number = 0;
+      const length: number = items.length;
+  
+      for (let i = 0; i < length; i++) {
+        console.log(`Processing item ${i + 1}/${length}`, items[i]);
+  
+        for (const key in items[i]) {
+          if (items[i].hasOwnProperty(key)) {
+            items[i][key] += 1;
+          }
+        }
+  
+        currentProgress++;
+        craftProgress = currentProgress / length;
+      }
+  
+      console.log(items)
 
-        return Items
+      return items;
 
     }
-
-}
-export default MargeService
+  }
+  
+  export default MargeService;
